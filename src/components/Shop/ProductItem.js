aeacf7b391b64f79e776555997a60078a5e10503
LewisCM14 <Lewiscmartin@live.co.uk>
2022-08-25 18:19:37 +0100

    create & wire up the addToCartHandler in ProductItem.js

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,28 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch } from "react-redux";
+
+import { cartActions } from "../../store/cart-slice";
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const dispatch = useDispatch(); // stores the useDispatch method in a variable
+
+  const { title, price, description, id } = props;
+
+  /**
+   * addToCartHandler, bound in the onClick event to the button in the Card below.
+   * uses the dispatch variable to call the addItemToCart method,
+   * found in the reducers of the cartSlice. id, title and price are passed as the payload
+   */
+  const addToCartHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
@@ -13,7 +33,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={addToCartHandler}>Add to Cart</button>
         </div>
       </Card>
     </li>
