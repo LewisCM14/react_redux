5fc91a930ce331cf52a170ff9a5a078ff45374fa
LewisCM14 <Lewiscmartin@live.co.uk>
2022-08-25 18:40:32 +0100

    wire up plus and minus button on CartItem.js

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,14 +1,42 @@
-import classes from './CartItem.module.css';
+import { useDispatch } from "react-redux";
+
+import classes from "./CartItem.module.css";
+import { cartActions } from "../../store/cart-slice";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const dispatch = useDispatch(); // stores the useDispatch method in a variable
+  const { title, quantity, total, price, id } = props.item;
+
+  /**
+   * uses the dispatch variable to call the removeItemFromCart method,
+   * found in the reducers of the cartSlice. Passes id as the payload.
+   * bound to the onClick event of the minus button below.
+   */
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  /**
+   * uses the dispatch variable to call the addItemFromCart method,
+   * found in the reducers of the cartSlice. Passes id, title and price as payload.
+   * bound to the onClick event of the plus button below.
+   */
+  const addItemHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
+          ${total.toFixed(2)}{" "}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
@@ -17,8 +45,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
