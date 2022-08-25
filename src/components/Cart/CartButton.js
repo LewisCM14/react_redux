e6acab13865f51505ac58e8bd9f8006ee4cf4efa
LewisCM14 <Lewiscmartin@live.co.uk>
2022-08-25 17:17:21 +0100

    bind the toggle action for ui-slice to the CartButton.js

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,21 @@
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
 
+
 const CartButton = (props) => {
+    const dispatch = useDispatch(); // stores the useDispatch method in a variable
+    
+    /**
+     * uses the dispatch variable to call the toggle method,
+     * found in the reducers of the uiSlice.
+     */
+    const toggleCartHandler = () =>{
+        dispatch(uiActions.toggle());
+    };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
