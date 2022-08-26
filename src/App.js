127de948053a2bb4be26c91021d3ab8f890c5944
LewisCM14 <Lewiscmartin@live.co.uk>
2022-08-26 17:42:55 +0100

    handle sending cart data to firebase backend

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -10,6 +11,21 @@ function App() {
    * receives the current state and the target value to update it with
    */
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  // subscribes to to the redux store for latest cart
+  const cart = useSelector((state) => state.cart);
+
+  /**
+   * sends a HTTP request to firebase backend, PUT method replaces existing data
+   * sends the data stored in 'cart' above, re-evaluates whenever cart store changes.
+   * logic for updating cart remains in reducer, async is done outside of it.
+   */
+  useEffect(() => {
+    fetch("https://cart-1bad1-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart)
+    });
+  }, [cart]);
 
   return (
     <Layout>
