a06abb336e38b77c9dfa0fe34eda2998502e8b77
LewisCM14 <Lewiscmartin@live.co.uk>
2022-08-28 13:50:38 +0100

    move sendCartData function to cart-actions.js

---
+++ src/store/cart-actions.js
@@ -0,0 +1,55 @@
+import { uiActions } from "./ui-slice";
+
+/**
+ * custom thunk method, points to another async function immediately
+ * dispatches the sending notification before attempting to send a
+ * a HTTP request to firebase backend, PUT method replaces existing data
+ * sends the data stored in 'cart', re-evaluates whenever cart store changes.
+ * then awaits the result of sendRequest, if no error is thrown shows success notification,
+ * else displays the error notification
+ */
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://cart-1bad1-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
